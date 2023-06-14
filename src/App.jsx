bc0f6b5be3c4c9d32e0a9a09e97071b776ffa47d
kashif <kashif_saleem_23@hotmail.com>
2023-06-15 01:40:25 +0500

    Add pictures to her0 section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header />
-      <div className='container'>
-        <h2>
-          Hello
-        </h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, cumque! Unde nam quidem, et vel, veritatis cumque saepe modi fuga itaque, rerum ex neque est obcaecati natus eum odio repellendus?</p>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo numquam odit minus perferendis dolor. Incidunt fugit voluptatum, repellat excepturi aliquam unde, delectus ea deleniti nemo modi saepe cupiditate dolores obcaecati aspernatur nulla. Vero autem magnam voluptatibus aut doloremque corrupti quod.
-          </p>
+      <Header />
+
+      <div className="container-hero">
+        <div className="intro">hello</div>
+        <div className="pictures-section">
+          <div className="shape1"></div>
+          <div className="shape2"></div>
+          <div className="shape3"></div>
+          <div className="shape4"></div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
