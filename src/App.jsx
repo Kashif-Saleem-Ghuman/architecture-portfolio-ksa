2f60c280a784e4579b4d95bb49dcf0c2f6c13902
kashif <kashif_saleem_23@hotmail.com>
2023-06-20 18:41:01 +0500

    Fix vh for container-hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import { useEffect} from "react";
 import "./App.css";
 import Header from "./pages/Header/Header";
 import Hero from "./pages/Hero/Hero";
 import About from  "./pages/About/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  
   return (
     <>
       <Header />
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import {React, useEffect} from "react";
 import heroImage from "../../assets/cut-outs2.png";
 import "./Hero.css";
 const Hero = () => {
+  useEffect(() => {
+    const updateVh = () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    window.addEventListener('resize', updateVh);
+    updateVh();
+    return () => {
+      window.removeEventListener('resize', updateVh);
+    };
+  }, []);
   return (
     <div className="wrapper">
       <div className="container-hero">
