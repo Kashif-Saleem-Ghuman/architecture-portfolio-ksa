bba1d22d02243becdcc203c27e1f7512a9635465
kashif <kashif_saleem_23@hotmail.com>
2023-06-20 15:54:16 +0500

    Add css into it's relatove file and folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import viteLogo from "/vite.svg";
 import heroImage from "./assets/cut-outs2.png";
 import "./App.css";
-import Header from "./Header";
+import Header from "./pages/Header/Header";
+import Hero from "./pages/Hero/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,21 +11,8 @@ function App() {
   return (
     <>
       <Header />
-      <div className="wrapper">
-        <div className="container-hero">
-          <div className="intro">
-            <h3 className="title">Welcome to Our</h3>
-            <h1
-            className="h1-hero">DESIGN & ARCHITECTURE STUDIO</h1>
-            <div className="btn-wrapper">
-              <button className="btn-round-pink">GET IN TOUCH</button>
-            </div>
-          </div>
-          <div className="image-hero">
-            <img src={heroImage} alt="Hero Image" />
-          </div>
-        </div>
-      </div>
+      <Hero/>
+      
     </>
   );
 }
