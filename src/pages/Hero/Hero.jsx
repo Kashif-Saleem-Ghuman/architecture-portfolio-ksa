d8ad0914a91dd677bcde5106e572f1f92d7eb319
kashif <kashif_saleem_23@hotmail.com>
2023-06-24 15:21:12 +0500

    Add prop to Hero component

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import heroImage from "../../assets/cut-outs2.png";
 import "./Hero.css";
-const Hero = () => {
+
+const Hero = (props) => {
+  const [inTouch, setInTouch] = useState(false);
+  useEffect(() => {
+    const updateVh = () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    window.addEventListener('resize', updateVh);
+    updateVh();
+    return () => {
+      window.removeEventListener('resize', updateVh);
+    };
+  }, []);
   return (
     <div className="wrapper">
       <div className="container-hero">
         <div className="intro">
-          <h3 className="title">Welcome to Our</h3>
-          <h1 className="h1-hero">DESIGN & ARCHITECTURE STUDIO</h1>
+          <h3 className="title">Welcome to</h3>
+          <h1 className="h1-hero">{props.name} DESIGN & ARCHITECTURE STUDIO</h1>
           <div className="btn-wrapper">
-            <button className="btn-round-pink">GET IN TOUCH</button>
+            <button 
+            onClick={() => setInTouch(true)}
+            disabled={inTouch}
+            className="btn-round-pink">
+              {!inTouch? 'Get in touch': 'Thank you!'}
+              </button>
           </div>
         </div>
         <div className="image-hero">
